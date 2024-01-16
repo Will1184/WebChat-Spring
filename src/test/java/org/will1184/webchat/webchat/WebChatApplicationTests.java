1f2abc86e6685fccd610fa62645c925611d52179
Will1184 <brandonwilliammg@gmail.com>
2024-01-15 22:16:20 -0600

    feat: se agrega el inicio del proyecto usando dependencias de websocket, lombok para el uso del chat

---
+++ src/test/java/org/will1184/webchat/webchat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.will1184.webchat.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
